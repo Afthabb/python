e548c626e502362fb230dc098c86114f59be8550
Afthabb <afthabanwarsaheed2028@cs.sjcetpalai.ac.in>
2024-10-01 15:48:48 +0530

    Add files via upload

---
+++ std det.py
@@ -0,0 +1,11 @@
+'''
+python program to get the student details
+Author: Afthab Anwar
+DATE: 01-10-2024
+VERSION:1.0
+'''
+
+name=input("enter student name-")
+cgpa=(float(input('enter cgpa-')))
+p=cgpa*10
+print("hi",name,',your percentile is',p)
