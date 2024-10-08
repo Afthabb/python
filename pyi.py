f4eaee3c3e2db3f514bf6551701adf91a38de7aa
Afthabb <afthabanwarsaheed2028@cs.sjcetpalai.ac.in>
2024-10-08 15:03:16 +0530

    Add files via upload

---
+++ pyi.py
@@ -0,0 +1,5 @@
+stname='afthab'
+ndname='anwar'
+finname=stname+' '+ndname
+print(finname)
+print ("the substring is",finname[7:])
---
+++ python task 4.py
@@ -0,0 +1,10 @@
+num1=int(input("enter your 1st number:"))
+num2=int(input("enter your 2nd number:"))
+num3=int(input("enter your 3rd number:"))
+print("The sum of num1 and num2 is:", num1+num2)
+print("The difference when num2 is subtracted from num1 is:", num1-num2)
+print("The product of num1 and num2 is:", num1*num2)
+print("The quotient when num1 is divided by num2 is:", num1/num2)
+print("The remainder when num1 is divided by num2 is:", num1%num2)
+result= (num1 + num2) * num3 /2
+print("The result of (num1 + num2) * num3 / 2 is:",result)
